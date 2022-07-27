78fcdf022e6c44b978467836cfb134fa0c4d30f0
Eric K. Sender <eric.sender@4tug.com>
2022-07-26 17:17:21 -0700

    Created github action to run maven
    - Maven will build then run lotto sim
    - Misc clean up

--- src/main/java/com/ericsender/lotto/Simulator.java
+++ src/main/java/com/ericsender/lotto/Simulator.java
@@ -9,7 +9,7 @@ import java.util.stream.Stream;
 
 public class Simulator {
 
-    private static int[] copyRangeAndSort(final int[] arr, final int from, final int to) {
+    private static int[] copyRangeThenSort(final int[] arr, final int from, final int to) {
         final var output = Arrays.copyOfRange(arr, from, to);
         Arrays.sort(output);
         return output;
@@ -20,7 +20,7 @@ public class Simulator {
         final var right = IntStream.rangeClosed(1, 25).toArray();
         Stream.of(left, right).forEach(ArrayUtils::shuffle);
         // My quick picked numbers:
-        final var leftWinner = new int[] {3, 29, 36, 50, 53}; //copyRangeAndSort(left, 0, 5);
+        final var leftWinner = new int[] {3, 29, 36, 50, 53}; //copyRangeThenSort(left, 0, 5);
         final var rightWinner = 13; //right[0];
 
         var leftTry = new int[0];
@@ -29,13 +29,13 @@ public class Simulator {
         final var sw = StopWatch.createStarted();
         while (!Arrays.equals(leftWinner, leftTry) || rightTry != rightWinner) {
             Stream.of(left, right).forEach(ArrayUtils::shuffle);
-            leftTry = copyRangeAndSort(left, 0, 5);
+            leftTry = copyRangeThenSort(left, 0, 5);
             rightTry = right[0];
-            if (tries++ % 100_000_000L == 0) System.out.printf("So far tries: %,d (trying for %s:%d - time %s)%n",
+            if (tries++ % 100_000_000L == 0) System.out.printf("So far tried: %,d (trying for %s:%d - time %s)%n",
                     tries - (tries > 1 ? 1 : 0), Arrays.toString(leftWinner), rightWinner, sw);
         }
 
-        System.out.printf("Get a winner matching %s:%d (%s:%d) in %,d tries in time %s.%n",
+        System.out.printf("Got a winner matching %s:%d (%s:%d) in %,d tries in time %s.%n",
                 Arrays.toString(leftWinner), rightWinner, Arrays.toString(leftTry), rightTry, tries, sw);
         return tries;
     }
